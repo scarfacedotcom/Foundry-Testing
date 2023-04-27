e876b9c9d3c3bd8bee1d8ea59cc9a95f2e301598
scarfacedotcom <peteradaaku@gmail.com>
2023-04-27 17:14:46 +0100

    .

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -10,6 +10,7 @@ contract CounterTest is Test {
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+        console.log(address(counter));
     }
 
     function testIncrement() public {
